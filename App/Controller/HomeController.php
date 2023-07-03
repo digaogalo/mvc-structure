0d79cc63bf75c36855f83334a953eb32e59b0fb9
digaogalo <digaolimac@gmail.com>
2023-07-03 11:44:39 -0300

    mvc-project

---
+++ App/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+class HomeController
+{
+    public function index()
+    {
+        try {
+            $colectPostagens = Postagem::selecionaTodos();
+            var_dump($colectPostagens);
+        } catch (Exception $e) {
+            echo $e-> getMessage();
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'App/Core/core.php';
+
+require_once 'lib/Database/Connection.php';
+
+require_once 'App/Controller/HomeController.php';
+require_once 'App/Controller/ErroController.php';
+
+require_once 'App/Model/Postagem.php';
+
+$template = file_get_contents('app/template/estrutura.html');
+
+ob_start();
+    $core = new Core;
+    $core->start($_GET);
+
+    $saida = ob_get_contents();
+ob_end_clean();
+
+$tplPronto = str_replace('{{area_dinamica}}',$saida, $template);
+
+echo $tplPronto;
